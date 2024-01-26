f8649c12f5602d9fb70dadc5343265619ecda4de
Elodie2Elodie <152405626+Elodie2Elodie@users.noreply.github.com>
2024-01-26 12:21:06 +0000

    Update DemoJenkinsApplication.java
    commitTestTrigger

--- src/main/java/com/groupeisi/demoJenkins/DemoJenkinsApplication.java
+++ src/main/java/com/groupeisi/demoJenkins/DemoJenkinsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoJenkinsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Bienvenue !!!");
 		SpringApplication.run(DemoJenkinsApplication.class, args);
 	}
 
