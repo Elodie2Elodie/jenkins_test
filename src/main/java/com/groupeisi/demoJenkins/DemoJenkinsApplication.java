3e7bc9742c45be819d240f85caa3059620e45a71
Elodie2Elodie <152405626+Elodie2Elodie@users.noreply.github.com>
2024-01-27 00:03:46 +0000

    Update DemoJenkinsApplication.java

--- src/main/java/com/groupeisi/demoJenkins/DemoJenkinsApplication.java
+++ src/main/java/com/groupeisi/demoJenkins/DemoJenkinsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoJenkinsApplication {
 
 	public static void main(String[] args) {
-		System.out.println(" Test Dépôt Nexus!!!");
+		System.out.println(" Effectuer une pipeline comportant Nexus!!!");
 		SpringApplication.run(DemoJenkinsApplication.class, args);
 	}
 
