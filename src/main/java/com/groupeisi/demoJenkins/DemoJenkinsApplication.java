6575b52f08916e38bc679a0c46f252840cb61c8b
Elodie <elodiedragneer@gmail.com>
2024-01-28 02:22:04 +0000

    commit concernant le test de pipeline avec nexus

--- src/main/java/com/groupeisi/demoJenkins/DemoJenkinsApplication.java
+++ src/main/java/com/groupeisi/demoJenkins/DemoJenkinsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoJenkinsApplication {
 
 	public static void main(String[] args) {
-		System.out.println(" La pipeline doit fonctionner, merci !!!");
+		System.out.println(" Cette pipeline doit fonctionner !!!");
 		SpringApplication.run(DemoJenkinsApplication.class, args);
 	}
 
