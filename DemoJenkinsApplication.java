1d580b75e367fa241ec7372c55ad81f9e82c17b8
Elodie <elodiedragneer@gmail.com>
2024-01-29 23:13:21 +0000

    commit nexus test

---
+++ DemoJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.groupeisi.demoJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Esperons que ca fonctionne!!!");
+		SpringApplication.run(DemoJenkinsApplication.class, args);
+	}
+
+}
