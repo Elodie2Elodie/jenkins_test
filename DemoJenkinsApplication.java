81152bc96714dc4e42d5adaae7293cbed22425eb
Elodie <elodiedragneer@gmail.com>
2024-01-29 23:27:39 +0000

    commit pour terter Nexus 2

--- DemoJenkinsApplication.java
+++ DemoJenkinsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoJenkinsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Fonctionne please, merci!!!");
+		System.out.println("Fonctionne please, merci bien!!!");
 		SpringApplication.run(DemoJenkinsApplication.class, args);
 	}
 
